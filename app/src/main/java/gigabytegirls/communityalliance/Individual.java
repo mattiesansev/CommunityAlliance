5ee218ce7fbab2431ed660b0dc2e73f263c8dcc8
mattiesansev <mesansev@gmail.com>
2017-04-22 20:12:32 -0700

    more pages

--- app/src/main/java/gigabytegirls/communityalliance/Individual.java
+++ app/src/main/java/gigabytegirls/communityalliance/Individual.java
@@ -1,7 +1,9 @@
 package gigabytegirls.communityalliance;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Individual extends AppCompatActivity {
 
@@ -10,4 +12,26 @@ public class Individual extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_individual);
     }
+    public void food(View v){
+
+    }
+    public void shelter(View v){
+
+    }
+    public void ally(View v){
+
+    }
+    public void money(View v){
+
+    }
+    public void onNext(View v){
+        Intent i = new Intent(Individual.this, details.class);
+        startActivity(i);}
+     public void go(View v) {
+         Intent i = new Intent(Individual.this, AUserSpace.class);
+         startActivity(i);
+     }
+
+
+
 }
